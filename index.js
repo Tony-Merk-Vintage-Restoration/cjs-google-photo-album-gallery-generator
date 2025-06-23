fc50f05dbfd09b9c7d99465b9e7f31ccab6dbc57
CJ Burkey <cjburkey01@gmail.com>
2025-06-22 22:05:54 -0400

    I think it's good

--- index.js
+++ index.js
@@ -1,42 +1,80 @@
 window.onload = main;
 
-async function main() {
+function main() {
   console.log("Initializing...");
 
-  let shareLinkInput = document.getElementById("google-photos-share-link");
+  let inputEmbedCode = document.getElementById("input-embed-code");
   let inputForm = document.getElementById("outer-generate-input-form");
   let outputTextarea = document.getElementById("generated-code-output");
 
-  if (!shareLinkInput || !inputForm || !outputTextarea) {
+  if (!inputEmbedCode || !inputForm || !outputTextarea) {
     console.error("Missing elements!");
-    console.debug(shareLinkInput);
+    console.debug(inputEmbedCode);
     console.debug(inputForm);
     console.debug(outputTextarea);
+    alert("Failed to initialize?!?!");
   }
 
-  inputForm.onsubmit = async (e) => {
-    e.preventDefault();
-    let albumLink = shareLinkInput.value;
-    if (!albumLink) {
-      console.warn("No Google Album URL provided");
-      return;
-    }
-    await generateForAlbum(albumLink);
-  };
+  inputForm.onsubmit = submitFn(inputEmbedCode, outputTextarea);
 
   console.log("Initialized!");
 }
 
-async function generateForAlbum(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
+function submitFn(inputEmbedCode, outputTextarea) {
+  return (event) => {
+    console.log("Transform input");
 
-    const txt = await response.text();
-    console.log(txt);
-  } catch (error) {
-    console.error(error.message);
+    event.preventDefault();
+    event.stopPropagation();
+
+    let inputCodeStr = inputEmbedCode.value?.trim();
+    if (!inputCodeStr) {
+      console.warn("No input embed code provided!");
+    } else {
+      onSubmit(inputCodeStr.trim(), outputTextarea);
+    }
+  };
+}
+
+function onSubmit(inputCodeStr, outputTextarea) {
+  // Get list of <object>'s from input
+  let galleryData = inputCodeStr.match(
+    /<div[^]*?class="pa-gallery-player-widget"[^]*?>\s*([^]*?)\s*<\/div>/,
+  );
+  if (!galleryData || !galleryData[1]) {
+    console.error("Failed to match on gallery data!");
+    return;
   }
+  let galleryDataStr = galleryData[1];
+
+  // Replace the <object>'s with <li><img>'s using the `transformObjToImg` function
+  let outputGalleryData = galleryDataStr.replaceAll(
+    /\s*<object[^]*?<\/object>\s*/g,
+    transformObjToImg,
+  );
+
+  // Write output with template
+  outputTextarea.value = `
+<div class="glide">
+    <div class="glide__track" data-glide-el="track">
+        <ul class="glide__slides">
+${outputGalleryData}
+        </ul>
+    </div>
+</div>
+  `.trim();
+}
+
+function transformObjToImg(objTagStr) {
+  console.debug(`Transform: ${objTagStr}`);
+
+  // Extract image URL
+  let dataUrl = objTagStr.match(/data="(.*?)"/);
+  if (!dataUrl || !dataUrl[1]) {
+    console.error('Failed to get image from object tag: "${objTagStr}"');
+    return "";
+  }
+
+  // Return carousel element with template
+  return `            <li class="glide__slide"><img src="${dataUrl[1]}" /></li>\n`;
 }
